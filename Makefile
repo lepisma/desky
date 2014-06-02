e7b5df1957157bb605a73aed27d4a4faf4e50d62
lepisma <abhinav.tushar.vs@gmail.com>
2014-06-03 02:31:22 +0530

    Add basic code to read config and frame the web app

--- Makefile
+++
@@ -1,5 +0,0 @@
-ui.py: design.ui
-	pyuic4 design.ui -o ui.py
-	
-clean:
-	rm ui.py
--- desky.py
+++ desky.py
@@ -1,27 +1,97 @@
 """
 Desky
+-----
+
+Wrap your web app in desktop frame
 """
 
-import sys
-from PyQt4 import QtGui, QtCore, Qt
-from ui import Ui_MainWindow
+import sys, subprocess
 
-class Gui(QtGui.QMainWindow):
-	def __init__(self, url = "http://localhost", parent=None):
-		QtGui.QWidget.__init__(self,parent)
-		self.ui = Ui_MainWindow()
-		self.ui.setupUi(self)
-		self.update
-		self.ui.webView.setUrl(QtCore.QUrl(url))
+from PyQt4.Qt import *
+
+import json
+
+class Desky(QWidget):
+	"""
+	The GUI Class
+	Opens the url in qt webview
+	"""
+
+	def __init__(self, url, name, server_process):
+		"""
+		Parameters
+		----------
+		url : string
+			The url to load in the frame
+		name : string
+			The name of frame (to be shown as window title)
+		server_process : subprocess.Popen or bool
+			The process which is handling the webpage
+		"""
+
+		QWidget.__init__(self)
+		self.setWindowTitle(name)
+		self.layout = QGridLayout(self)
+		self.layout.setMargin(0)
+		self.layout.setSpacing(0)
+
+		self.view = QWebView(self)
+		self.view.setUrl(QUrl(url))
+
+		self.layout.addWidget(self.view, 0, 0, 1, 1)
+
+		self.server_process = server_process
+
+	def closeEvent(self, event):
+		"""
+		Kills the server process and quits
+		"""
 		
-	def close(self):
-		sys.exit()
+		if self.server_process != False:
+			self.server_process.kill()
+
+		event.accept()
 		
 def main():
-	app = QtGui.QApplication(sys.argv)
-	ex = Gui()
-	ex.show()
-	sys.exit(app.exec_())
+	"""
+	Main function
+	Scans directory for desky_config.json
+	Runs server (if needed)
+	Passes URL to qt webkit
+	"""
+
+	try:
+		config = json.load(open('desky_config.json', 'rb'))
+	except IOError as e:
+		if e.errno == 2:
+			print "Config file not found"
+		else:
+			print "Something wicked happened while reading config"
+		sys.exit()
+
+	try:
+		cmd = config['cmd']
+		server_process = subprocess.Popen(cmd)
+	except KeyError:
+		server_process = False
+		print "No command to run, opening frame now"
+
+	try:
+		url = config['url']
+	except KeyError:
+		print "No url specified, exiting"
+		sys.exit()
+
+	try:
+		name = config['name']
+	except KeyError:
+		print "No name specified, using 'Desky'"
+		name = "Desky"
+	
+	app = QApplication(sys.argv)
+	frame = Desky(url, name, server_process)
+	frame.show()
+	app.exec_()
 
 if __name__ == '__main__':
 	main()
